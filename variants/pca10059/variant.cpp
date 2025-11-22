4a18e830fbfa3d03bef5ed75c1b43b642bfc295e
Josiah VanderZee <jvanderzee@apache.org>
2025-11-22 14:53:01 -0600

    Finish pin mapping in variant source file
    This maps unused pins to `0xff` and adds an `initVariant` function which
    the framework library will call to put pins in a safe initial state.

--- variants/pca10059/variant.cpp
+++ variants/pca10059/variant.cpp
@@ -20,15 +20,33 @@
 
 #include "variant.h"
 
+#include <Arduino.h>
+
+// Pins 19, 21, 23, and 25 are connected to SW2 and should not be used.
+// See the datasheet. Many other pins are not exposed to on the dongle.
 const uint32_t g_ADigitalPinMap[] =
 {
   // P0
-  0   , 1 , 2 , 0xff, 4   , 0xff, 6   , 0xff,
-  8   , 9 , 10, 11  , 12  , 13  , 14  , 15  ,
-  0xff, 17, 18, 19  , 20  , 21  , 22  , 23  ,
-  24  , 25, 26, 0xff, 0xff, 29  , 0xff, 31  ,
+  0   , 1   , 2 , 0xff, 4   , 0xff, 6   , 0xff,
+  8   , 9   , 10, 11  , 12  , 13  , 14  , 15  ,
+  0xff, 17  , 18, 0xff, 20  , 0xff, 22  , 0xff,
+  24  , 0xff, 26, 0xff, 0xff, 29  , 0xff, 31  ,
 
   // P1
-  32  , 33, 34, 0xff, 36, 0xff, 38, 39,
-  0xff, 41, 42, 43  , 44, 45  , 46, 47
+  32  , 33, 34, 0xff, 36,   0xff,   38, 39,
+  0xff, 41, 42, 43  , 0xff, 45  , 0xff, 47
 };
+
+void initVariant() {
+  pinMode(PIN_BUTTON1, INPUT_PULLUP);
+
+  pinMode(PIN_LED1, OUTPUT);
+  pinMode(LED_RED, OUTPUT);
+  pinMode(LED_GREEN, OUTPUT);
+  pinMode(LED_BLUE, OUTPUT);
+
+  digitalWrite(PIN_LED1, HIGH);
+  digitalWrite(LED_BLUE, HIGH);
+  digitalWrite(LED_GREEN, HIGH);
+  digitalWrite(LED_RED, HIGH);
+}
