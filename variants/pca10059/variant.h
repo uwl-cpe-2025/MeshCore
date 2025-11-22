069153c39059b58fe64787daf0ea268bd07ebbcb
Josiah VanderZee <jvanderzee@apache.org>
2025-11-22 14:55:03 -0600

    Add WIP changes to variant.h file

--- variants/pca10059/variant.h
+++ variants/pca10059/variant.h
@@ -64,13 +64,8 @@ extern "C"
 // This pin may be used to read the state of the on-chip push button.
 //
 // To use the button, configure this pin as an input with an internal
-// pull-up resistor as described in the datasheet.
-//
-// We have chosen to name the macro PIN_BUTTON_SW1 to disambiguate it
-// from the possibly common name PIN_BUTTON1, as a safeguard against
-// accidentally using the button without configuring it for the
-// internal pullup resistor.
-#define PIN_BUTTON_SW1       38
+// pull-up resistor. It will be configured this way by default.
+#define PIN_BUTTON1        (38)
 
 // These four pins may be used as analog input (AIN) pins.
 //
@@ -81,40 +76,69 @@ extern "C"
 #define PIN_A2               (29)
 #define PIN_A3               (31)
 
+#define ADC_RESOLUTION       (14)
+
 static const uint8_t A0  = PIN_A0 ;
 static const uint8_t A1  = PIN_A1 ;
 static const uint8_t A2  = PIN_A2 ;
 static const uint8_t A3  = PIN_A3 ;
 
-#define ADC_RESOLUTION    14
+// Everything above here has been corrected and documented.
+// Notes for future work:
+//
+//  PIN_SERIAL1_RX and PIN_SERIAL1_TX are used by UART
+//  Same for PIN_SERIAL2_RX and PIN_SERIAL2_TX
+//
+//  We may want Serial1 for Adafruit logging. MeshCore also uses it for environment sensors.
+//
+//  PIN_WIRE_SDA and PIN_WIRE_SCL are used by Wire library for I2C
+//
+//  I don't think we need I2C. :)
+//
+//  PIN_SPI_XXX are used by the SPI library
+//
+//  I think we do need SPI.
+//
+//  It looks to me like the T-Echo uses a QSPI external flash. We need to look into that. :/
+//
+//  PIN_BUTTON1 is used for the backlight in the T-Echo platformio.ini, we might do the same. PIN_BUTTON2 isn't used that I could find yet.
+//
+//  The SX126X pins are necessary and will be used to communicate with the radio module. These are important.
+//
+//  Looks like we do not use PIN_SPI1_XXX.
+//
+//  We need to set P_LORA_XXX in the platform file.
+//
+//  We do need to define some DISP_XXX pins for the GxEPDDisplay.
+//
+//  We do need some GPS_XXX pins for the MeshCore sensors.
+
 
 // Other pins
-#define PIN_AREF           (2)
-#define PIN_NFC1           (9)
-#define PIN_NFC2           (10)
+//#define PIN_AREF           (2)
+//#define PIN_NFC1           (9)
+//#define PIN_NFC2           (10)
 
-static const uint8_t AREF = PIN_AREF;
+//static const uint8_t AREF = PIN_AREF;
 
 /*
  * Serial interfaces
  */
 
 // Arduino Header D0, D1
-#define PIN_SERIAL1_RX      (33) // P1.01
-#define PIN_SERIAL1_TX      (34) // P1.02
-
-// Connected to Jlink CDC
-#define PIN_SERIAL2_RX      (8)
-#define PIN_SERIAL2_TX      (6)
+// TODO tentative
+#define PIN_SERIAL1_RX      (9)
+#define PIN_SERIAL1_TX      (10)
 
 /*
  * SPI Interfaces
  */
+// TODO tentative
 #define SPI_INTERFACES_COUNT 1
 
-#define PIN_SPI_MISO         (46)
-#define PIN_SPI_MOSI         (45)
-#define PIN_SPI_SCK          (47)
+#define PIN_SPI_MISO         (15)
+#define PIN_SPI_MOSI         (17)
+#define PIN_SPI_SCK          (20)
 
 static const uint8_t SS   = 44 ;
 static const uint8_t MOSI = PIN_SPI_MOSI ;
@@ -124,22 +148,27 @@ static const uint8_t SCK  = PIN_SPI_SCK ;
 /*
  * Wire Interfaces
  */
+// TODO tentative
 #define WIRE_INTERFACES_COUNT 1
 
-#define PIN_WIRE_SDA         (26)
-#define PIN_WIRE_SCL         (27)
+#define PIN_WIRE_SDA         (22)
+#define PIN_WIRE_SCL         (24)
 
 // QSPI Pins
+/*
 #define PIN_QSPI_SCK         19
 #define PIN_QSPI_CS          17
 #define PIN_QSPI_IO0         20
 #define PIN_QSPI_IO1         21
 #define PIN_QSPI_IO2         22
 #define PIN_QSPI_IO3         23
+*/
 
 // On-board QSPI Flash
+/*
 #define EXTERNAL_FLASH_DEVICES   MX25R6435F
 #define EXTERNAL_FLASH_USE_QSPI
+*/
 
 #ifdef __cplusplus
 }
